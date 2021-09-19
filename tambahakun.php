4d5300d7acb5f858dbe706d9cf17117142f32f97
ArrasM <muhammadaras4321@gmail.com>
2021-09-20 01:22:44 +0700

    memperbaiki tambah akun

--- tambahakun.php
+++ tambahakun.php
@@ -2,9 +2,9 @@
 <?php
 if(!empty($_POST["register"])) {
 	require_once("koneksi.php");
-	$sql = "INSERT INTO akun(nama_lengkap,email,password) VALUES (:nama_lengkap,:email,:password)";
+	$sql = "INSERT INTO akun(nama_lengkap,email,nomor_telepon,password) VALUES (:nama_lengkap,:email,:nomor_telepon,:password)";
 	$stmt = $pdo_conn->prepare( $sql );
-	$result = $stmt->execute( array(':nama_lengkap'=>$_POST['nama_lengkap'],':email'=>$_POST['email'],':password'=>$_POST['password']) );
+	$result = $stmt->execute( array(':nama_lengkap'=>$_POST['nama_lengkap'],':email'=>$_POST['email'],':nomor_telepon'=>$_POST['nomor_telepon'],':password'=>$_POST['password']) );
   if (!empty($result) ){
 	  header('location:index.html');
 	}else{
